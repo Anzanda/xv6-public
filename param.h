81460726335129164ed4eaf0e923197a3f4864e4
Anzanda <tjd1024@g.skku.edu>
2024-05-09 20:24:00 +0900

    feat: mmap()에 필요한 constant추가

--- param.h
+++ param.h
@@ -1,14 +1,21 @@
-#define NPROC        64  // maximum number of processes
-#define KSTACKSIZE 4096  // size of per-process kernel stack
-#define NCPU          8  // maximum number of CPUs
-#define NOFILE       16  // open files per process
-#define NFILE       100  // open files per system
-#define NINODE       50  // maximum number of active i-nodes
-#define NDEV         10  // maximum major device number
-#define ROOTDEV       1  // device number of file system root disk
-#define MAXARG       32  // max exec arguments
-#define MAXOPBLOCKS  10  // max # of blocks any FS op writes
-#define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
-#define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
-#define FSSIZE       1000  // size of file system in blocks
+#define NPROC 64                  // maximum number of processes
+#define KSTACKSIZE 4096           // size of per-process kernel stack
+#define NCPU 8                    // maximum number of CPUs
+#define NOFILE 16                 // open files per process
+#define NFILE 100                 // open files per system
+#define NINODE 50                 // maximum number of active i-nodes
+#define NDEV 10                   // maximum major device number
+#define ROOTDEV 1                 // device number of file system root disk
+#define MAXARG 32                 // max exec arguments
+#define MAXOPBLOCKS 10            // max # of blocks any FS op writes
+#define LOGSIZE (MAXOPBLOCKS * 3) // max data blocks in on-disk log
+#define NBUF (MAXOPBLOCKS * 3)    // size of disk block cache
+#define FSSIZE 1000               // size of file system in blocks
 
+#define PROT_READ 0x1
+#define PROT_WRITE 0x2
+
+#define MAP_ANONYMOUS 0x1
+#define MAP_POPULATE 0x2
+
+#define NMMAP 64 // maximum number of mmap areas
